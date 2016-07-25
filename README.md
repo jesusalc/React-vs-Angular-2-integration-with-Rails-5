588c15f1cb585666d13ac920945e47d20c69c980
jesusalc <jesusalc@gmail.com>
2016-07-25 19:14:33 +0200

    created Setting up a sample Rails application

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# React-vs-Angular-2-integration-with-Rails-5
-React vs. Angular 2 integration with Rails Read more at http://tutorials.pluralsight.com/ruby-ruby-on-rails/react-vs-angular-2-integration-with-rails#Ew83c8o3RcK6Xcdm.99
+# React vs. Angular 2 integration with Rails
+  
+ One of the most recent debates in the web development world has been over the choice of constructing the view layer of modern applications. In this guide, we will compare the React  and the Angular 2  integrations using Ruby on Rails. The two approaches will be analyzed in terms of ease of integration and use with Rails.
+ 
+ 
+ Read more at http://tutorials.pluralsight.com/ruby-ruby-on-rails/react-vs-angular-2-integration-with-rails#Ew83c8o3RcK6Xcdm.99
---
+++ config/application.rb
@@ -0,0 +1,32 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Sampleapp
+  class Application < Rails::Application
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+
+    #...Rack-Cors Configuration - Start
+    config.middleware.insert_before 0, 'Rack::Cors' do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
+    # This configuration will permit all types of
+    # requests from any source (* denotes any address)
+    # to be accepted. For development, this setup is
+    # acceptable. However, for production, you need to
+    # replace the asterisk with the URL of your client-side
+    # application.
+
+    #...Rack-Cors Configuration - end
+
+  end
+end
