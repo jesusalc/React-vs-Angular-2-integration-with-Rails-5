d6fd945ced8064e4eb8e59cc6a2a4f4d554a954e
jesusalc <jesusalc@gmail.com>
2016-07-25 21:04:18 +0200

    added Open the file and put the following code

--- app/assets/javascripts/components/item.es6.jsx
+++ app/assets/javascripts/components/item.es6.jsx
@@ -1,6 +1,16 @@
+//app/assets/javascripts/components/item.es6.jsx
 class Item extends React.Component {
-  render () {
-    return <div />;
-  }
-}
+    constructor(props, context) {
+        super(props, context);
+        this.state = {item: ''};
+    };
 
+    render () {
+        return (
+            <div>
+                <h2> Rails React starter </h2>
+                <div> {this.state.item}</div>
+            </div>
+        );
+    }
+}
