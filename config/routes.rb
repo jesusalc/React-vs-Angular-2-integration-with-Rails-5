37e3779eca03e25905069c8bf4f69c55110ff337
jesusalc <jesusalc@gmail.com>
2016-07-25 20:44:07 +0200

    added a react route for site_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/api' => 'application#index', defaults: { format: :json }
+  root to: 'site#index' #add the route for the React component
 end
